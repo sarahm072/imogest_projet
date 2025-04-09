0605d3381859e9ae961ee5218345d4001a93a8ce
Sara Meraouche <148572793+sarahm072@users.noreply.github.com>
2025-04-09 20:07:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,33 @@
-let images1 = ["./image/maisson/home1-1.jpg', './image/maisson/home1-2.jpg"];
-let images2 = ["./image/maisson/home2-1.jpg', './image/maisson/home2-2.jpg"];
+let images1 = ["./image/maisson/home1-1.jpg", "./image/maisson/home1-2.jpg"];
+let images2 = ["./image/maisson/home2-1.jpg", "./image/maisson/home2-2.jpg"];
 
-
+let currentImages = [];
 let currentIndex = 0;
 
 function openModal1() {
+    currentImages = images1;
+    currentIndex = 0;
     document.getElementById("photoModal").style.display = "flex";
-    document.getElementById("modalImage").src = images1[currentIndex];
+    document.getElementById("modalImage").src = currentImages[currentIndex];
 }
 
 function openModal2() {
+    currentImages = images2;
+    currentIndex = 0;
     document.getElementById("photoModal").style.display = "flex";
-    document.getElementById("modalImage").src = images1[currentIndex];
+    document.getElementById("modalImage").src = currentImages[currentIndex];
 }
+
 function closeModal() {
     document.getElementById("photoModal").style.display = "none";
 }
 
 function nextImage() {
-    currentIndex = (currentIndex + 1) % images.length;
-    document.getElementById("modalImage").src = images[currentIndex];
+    currentIndex = (currentIndex + 1) % currentImages.length;
+    document.getElementById("modalImage").src = currentImages[currentIndex];
 }
 
 function prevImage() {
-    currentIndex = (currentIndex - 1 + images.length) % images.length;
-    document.getElementById("modalImage").src = images[currentIndex];
+    currentIndex = (currentIndex - 1 + currentImages.length) % currentImages.length;
+    document.getElementById("modalImage").src = currentImages[currentIndex];
 }
